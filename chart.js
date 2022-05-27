7541e8246da68e4924813fef86c3303aa0263538
dojunmin <105894706+dojunmin@users.noreply.github.com>
2022-05-27 13:03:40 +0900

    유입 차트 추가

--- chart.js
+++ chart.js
@@ -2,6 +2,9 @@
     title: {
       text: "LG CNS HomePage Customer journey analysis",
     },
+    subtitle: {
+      text: '2022.05.16 ~ 2022.05.20'
+    },
     accessibility: {
       point: {
         valueDescriptionFormat:
@@ -10,21 +13,40 @@
     },
     series: [
       {
-        keys: ["from", "to", "weight", "labelrank"],
+        keys: ["from", "to", "weight"],
         data: [
-          ["naver", "메인", 1065],
-          ["google", "메인", 812],
-          ["다이렉트", "메인", 639],
-          ["naver/링크", "메인", 162],
+          // ["naver", "메인", 1065],
+          // ["google", "메인", 812],
+          // ["다이렉트", "메인", 639],
+          // ["naver/링크", "메인", 162],
           // ["naver", "인재채용", 176, 1],
           // ["google", "인재채용", 243, 1],
           // ["다이렉트", "인재채용", 47, 1],
-          // ["naver/링크", "인재채용", 72, 1],
-          ["메인", "인재채용", 405],
+          // ["naver/링크", "인재채용", 72, 1],          
           //["메인", "신입사원/하계인턴 채용", 211],
-          ["메인", "회사소개", 336],
-          ["메인", "DX자료실", 145],
           ["메인", "비즈니스/클라우드", 139],
+          ["메인", "DX자료실", 145],
+          ["메인", "회사소개", 336],          
+          ["메인", "인재채용", 405],
+          ["비즈니스/클라우드", "문의하기", 35],
+          ["비즈니스/클라우드", "CloudXper", 20],
+          ["비즈니스/클라우드", "CloudXper ProOps", 19],
+          ["비즈니스/클라우드", "데이터센터", 19],
+          ["DX자료실", "문의하기", 15],
+          ["DX자료실", "미션 잉글리시", 110],
+          ["DX자료실", "LISA 스피킹 클래스", 69],
+          ["DX자료실", "LISA TOST", 79],
+          ["DX자료실", "하루조각", 97],
+          ["회사소개", "문의하기", 33],
+          ["회사소개", "CEO소개", 21],
+          ["회사소개", "History", 53],
+          ["회사소개", "Vision", 29],
+          ["회사소개", "투자정보", 269],          
+          ["인재채용", "문의하기", 40],
+          ["인재채용", "DX 지원하기", 205],
+          ["인재채용", "스마트팩토리 지원하기", 150],
+          ["인재채용", "인턴(학사)", 78],          
+          ["인재채용", "인턴(석/박사)", 59],
           // ["인재채용", "메인", 92],
           // ["인재채용", "DX자료실", 8],
           // ["인재채용", "회사소개", 14],        
@@ -37,11 +59,12 @@
           // ["DX자료실", "회사소개", 15],        
           // ["DX자료실", "비즈니스/클라우드", 14]
         ],
-        events: {
+        events: {          
           click: function (event) {
             if (event.point.name === "메인") {
-              drawWordCloud();
+              drawChart();
               drawGraph();
+              drawWordCloud();              
               document.getElementById(
                 "beausable"
               ).innerHTML = `<img src=".\\main_heatmap.png" style="width:30%; height:30%;"></img>`
@@ -58,6 +81,9 @@
               ).innerHTML = `<img src=".\\career_heatmap.png" style="width:30%; height:30%;"></img>`
               + `<img src=".\\career_move.png" style="width:30%; height:30%; margin-left:3%;"></img>`
                 + `<img src=".\\career_scroll.png" style="width:30%; height:30%; margin-left:3%;"></img>`;
+              // document.getElementById("chart").style.height = "0px";
+              // document.getElementById("graph").style.height = "0px";
+              // document.getElementById("word_cloud").style.height = "0px";              
               // document.getElementById("vital_main").style.height = "0px";
               // document.getElementById("vital_career").style.visibility = "visible";
               // document.getElementById("vital_company").style.height = "0px";
@@ -155,9 +181,9 @@
       title: {
         text: 'LG CNS 홈페이지 Web Vitals'
       },
-      // subtitle: {
-      //   text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
-      // },
+      subtitle: {
+        text: '2022.05.16 ~ 2022.05.20'
+      },
       xAxis: {
         categories: ['LCP', 'FID', 'CLS'],
         title: {
@@ -218,5 +244,62 @@
         name: '비즈니스/클라우드',
         data: [1.3, 0.8, 0.09]
       }]
+    });
+  }
+
+  function drawChart() {
+    Highcharts.chart('chart', {
+
+      chart: {
+        type: 'item'
+      },
+    
+      title: {
+        text: 'LG CNS 홈페이지 유입 차트'
+      },
+    
+      subtitle: {
+        text: '2022.05.16 ~ 2022.05.20'
+      },
+    
+      legend: {
+        labelFormat: '{name} <span style="opacity: 0.4">{y}</span>'
+      },
+    
+      series: [{
+        name: 'Representatives',
+        keys: ['name', 'y', 'color', 'label'],
+        data: [
+          ['Naver/검색', 1065, '#64A12D', 'Naver'],
+          ['Google/검색', 812, '#009EE0', 'Google'],
+          ['직접 유입', 639, '#FFED00', 'Direct'],
+          ['Naver 링크', 162, '#EB001F', 'Naver/링크']
+        ],
+        dataLabels: {
+          enabled: true,
+          format: '{point.label}'
+        },
+    
+        // Circular options
+        center: ['50%', '88%'],
+        size: '170%',
+        startAngle: -100,
+        endAngle: 100
+      }],
+    
+      responsive: {
+        rules: [{
+          condition: {
+            maxWidth: 600
+          },
+          chartOptions: {
+            series: [{
+              dataLabels: {
+                distance: -30
+              }
+            }]
+          }
+        }]
+      }
     });
   }
